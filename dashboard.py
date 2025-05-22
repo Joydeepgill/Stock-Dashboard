6e14037840af652e92181b223fe9b057357922cd
Joydeep Gill <joyydeep.gill@gmail.com>
2025-05-21 22:51:46 -0400

    Added basic skeleton functions

---
+++ dashboard.py
@@ -0,0 +1,15 @@
+class Dashbaord: 
+
+    def __init__(): 
+
+    
+    def parse_csv(): 
+        ''' 
+        Extract and parse data from the CSV file 
+        '''
+    
+    
+    def plot(): 
+        ''' 
+        plot the extracted data into a graph 
+        ''' 
